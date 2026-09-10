1c3289513e970039c42cfea2931daefa1fd02371
Anjali <njlipatel@gmail.com>
2026-09-10 22:08:43 +0530

    simplify

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
-/**
- * AngularJS Application Bootstrap
- * Defines main module 'angularLearnApp'
- */
-(function() {
-    'use strict';
+// 1. Create the AngularJS application module
+var app = angular.module("myApp", []);
 
-    angular.module('angularLearnApp', []);
-})();
+// 2. Create the Controller
+// - $scope: bridges JavaScript data with the HTML view
+// - $http: built-in tool to fetch data from our JSON file or server
+app.controller("MainController", function($scope, $http) {
 
+    // Load data from data.json
+    $http.get("data.json").then(function(response) {
+        // response.data holds the contents of our data.json file
+        $scope.profile = response.data.profile;
+        $scope.sections = response.data.sections;
+    });
+
+});
